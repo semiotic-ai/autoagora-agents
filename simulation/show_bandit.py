6d253d89e7dc3d841a6827df387905eccd2352f8
Tomasz Kornuta <tomasz@semiotic.ai>
2022-08-11 15:06:19 -0700

    new (elementary) configuration management, refactored bandit scripts, operational
    Signed-off-by: Tomasz Kornuta <tomasz@semiotic.ai>

---
+++ simulation/show_bandit.py
@@ -0,0 +1,136 @@
+# Copyright 2022-, Semiotic AI, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+from asyncio import run
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+from agents.agent_factory import add_agent_argparse
+from environments.environment_factory import add_environment_argparse
+from simulation.controller import init_simulation
+
+
+def add_experiment_argparse(parser: argparse.ArgumentParser):
+    """Adds argparse arguments related to experiment to parser."""
+    parser.add_argument(
+        "-c",
+        "--config",
+        default="simulation/configs/3different_agents_noisy_cyclic.json",
+        type=str,
+        help="Sets the config file (DEFAULT: simulation/configs/3different_agents_noisy_cyclic.json)",
+    )
+    parser.add_argument(
+        "-i",
+        "--iterations",
+        default=3500,
+        type=int,
+        help="Sets the length of the experiment / number of iterations (DEFAULT: 3500)",
+    )
+    parser.add_argument(
+        "-f",
+        "--fast-forward-factor",
+        default=20,
+        type=int,
+        help="Sets the fast forward factor (DEFAULT: 50)",
+    )
+    parser.add_argument(
+        "--show", action="store_true", help="If set, shows the animation"
+    )
+    parser.add_argument(
+        "--save", action="store_true", help="If set, saves the animation to a file"
+    )
+
+
+if __name__ == "__main__":
+    # Init argparse.
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [-c ...] [-e ...] [-i ...] [--show] [--save]",
+        description="Runs agent simulation and (optionally) shows it and/or saves it to a file.",
+    )
+    add_experiment_argparse(parser=parser)
+    add_agent_argparse(parser=parser)
+    add_environment_argparse(parser=parser)
+
+    # Initialize the simulation.
+    args, environment, agents = init_simulation(parser=parser)
+    bandit = next(iter(agents.values()))
+
+    # Generate the filename.
+    FILENAME = f"{args.config}.mp4"
+
+    fig, ax = plt.subplots()
+    container = []
+
+    # Environment x.
+    min_x = 1e-10
+    max_x = 5e-6
+
+    print(f"Training {bandit} on {environment}. Please wait...")
+    for i in range(args.iterations):
+        # X. Collect the values for visualization of non-stationary environment: subgraph queries/price plot.
+        if i % args.fast_forward_factor == 0:
+            # Plot environment.
+            env_x, env_y = run(environment.generate_plot_data(min_x, max_x))
+            (im_env,) = plt.plot(env_x, env_y, color="grey")
+
+        # 1. Get bid from the agent (action)
+        scaled_bid = bandit.get_action()
+
+        # 2. Act: set multiplier in the environment.
+        run(environment.set_cost_multiplier(scaled_bid))
+
+        # 3. Get the reward.
+        # Get queries per second.
+        queries_per_second = run(environment.queries_per_second())
+        # Turn it into "monies".
+        monies_per_second = queries_per_second * scaled_bid
+        # Add reward.
+        bandit.add_reward(monies_per_second)
+
+        # 4. Update the policy.
+        loss = bandit.update_policy()
+
+        # X. Collect the values for visualization of agent's gaussian policy.
+        if i % args.fast_forward_factor == 0:
+            agent_x, agent_y, init_agent_y = run(
+                bandit.generate_plot_data(min_x, max_x)
+            )
+            (img_agent,) = plt.plot(agent_x, agent_y, color="b")
+            (img_init_agent,) = plt.plot(agent_x, init_agent_y, color="g")
+
+            # Put both "images" with labels & title into a container.
+            ax.set_xlabel("Price multiplier")
+            # ax.set_ylabel('queries/s ')
+            title = ax.text(
+                0.5,
+                1.05,
+                f"time {i}",
+                size=plt.rcParams["axes.titlesize"],
+                ha="center",
+                transform=ax.transAxes,
+            )
+            legend = ax.legend([im_env, img_agent, img_init_agent], ["Queries/s", "Policy PDF", "Init Policy PDF"])  # type: ignore
+            container.append([im_env, img_agent, title])  # type: ignore
+
+        # 5. Make a step.
+        environment.step()
+
+    if args.show or args.save:
+        print("Plotting animation...")
+        ani = animation.ArtistAnimation(
+            fig, container, interval=50, blit=False, repeat_delay=1000
+        )
+
+        # Set up formatting for the movie files.
+        Writer = animation.writers["ffmpeg"]
+        writer = Writer(fps=15, metadata=dict(artist="Me"), bitrate=1800)
+
+        if args.show:
+            plt.show()
+
+        if args.save:
+            print(f"Saving movie, please wait...")
+            ani.save(FILENAME, writer=writer)
+            print(f"Movie saved to '{FILENAME}'")
---
+++ simulation/train_bandit.py
@@ -0,0 +1,107 @@
+# Copyright 2022-, Semiotic AI, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import os
+import sys
+
+import numpy as np
+from anyio import run
+from torch.utils.tensorboard.writer import SummaryWriter
+
+from agents.agent_factory import AgentFactory, add_agent_argparse
+from environments.environment_factory import (
+    EnvironmentFactory,
+    add_environment_argparse,
+)
+
+
+def add_experiment_argparse(parser: argparse.ArgumentParser):
+    """Adds argparse arguments related to experiment to parser."""
+    parser.add_argument(
+        "-i",
+        "--iterations",
+        default=3500,
+        type=int,
+        help="Sets the length of the experiment / number of iterations (DEFAULT: 3500)",
+    )
+
+
+try:
+    import socket
+    from datetime import datetime
+
+    current_time = datetime.now().strftime("%b%d_%H-%M-%S")
+    log_dir = os.path.join(
+        "runs", current_time + "_" + socket.gethostname() + sys.argv[1]
+    )
+
+    writer = SummaryWriter(log_dir)
+except:
+    writer = SummaryWriter()
+
+
+async def main_loop():
+    vars = {"GLOBAL_COST_MULTIPLIER": 0.000001, "DEFAULT_COST": 50}
+
+    # Init argparse.
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [-a ...] [-e ...] [-n ...]",
+        description="Trains an agent on a given environment.",
+    )
+    add_experiment_argparse(parser=parser)
+    add_agent_argparse(parser=parser)
+    add_environment_argparse(parser=parser)
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Instantiate the agent.
+    bandit = AgentFactory(
+        agent_type=args.agent,
+        learning_rate=args.learning_rate,
+        buffer_max_size=args.buffer_size,
+    )
+
+    # Instantiate the environment.
+    environment = EnvironmentFactory(environment_type=args.environment)
+
+    total_money = 0
+    print(f"Training {bandit} on {environment}. Please wait...")
+    for i in range(args.iterations):
+        writer.add_scalar("Distribution mean", bandit.mean, i)
+        writer.add_scalar("Distribution stddev", bandit.logstddev.exp(), i)
+
+        # 1. Get bid from the agent (action)
+        scaled_bid = bandit.get_action()
+        writer.add_scalar("Price multiplier", scaled_bid, i)
+
+        # 2. Act: set multiplier in the environment.
+        await environment.set_cost_multiplier(scaled_bid)
+
+        # 3. Get the reward.
+        # Get queries per second.
+        queries_per_second = await environment.queries_per_second()
+        writer.add_scalar("Queries per second", queries_per_second, i)
+
+        # Turn it into "monies".
+        monies_per_second = queries_per_second * scaled_bid
+        writer.add_scalar("Monies per second", monies_per_second, i)
+        total_money += monies_per_second
+        writer.add_scalar("Monies", total_money, i)
+
+        # Add reward.
+        bandit.add_reward(monies_per_second)
+
+        # 4. Update the policy.
+        loss = bandit.update_policy()
+        if loss is not None:
+            writer.add_scalar("Loss", loss, i)
+
+        # 5. Make a step.
+        environment.step()
+
+    print(f"Training finished. Logs saved to '{writer.log_dir}'")
+
+
+if __name__ == "__main__":
+    run(main_loop)
