19fa6330d7cb5071927fea7dfaf7fdb538b3074b
Tümay Tuzcu <tumay@semiotic.ai>
2023-01-02 14:25:13 +0300

    refactor(chart.py): replaced if value is not None and call with a single line value and call
    Signed-off-by: Tümay Tuzcu <tumay@semiotic.ai>

--- simulation/chart.py
+++ simulation/chart.py
@@ -18,7 +18,7 @@ class RGBAColor(NamedTuple):
         R (int): value between 0 and 255 for red value.
         G (int): value between 0 and 255 for green value.
         R (int): value between 0 and 255 for blue value.
-        A (int): value between 0 and 255 for alpha/opacity value.Default is `255`
+        A (int): value between 0 and 255 for alpha/opacity value. Default is `255`.
     """
 
     R: int
@@ -41,7 +41,7 @@ NamedColor = Literal[
 
 
 class IndexedColor(NamedTuple):
-    """Color from a single index. Useful for stepping through a predefined list of colors.
+    """Color from a single index. Useful for stepping through a predefined colors.
 
     Attributes:
         index (int): predefined color index
@@ -53,23 +53,34 @@ class IndexedColor(NamedTuple):
 
 
 Color = Union[RGBAColor, NamedColor, IndexedColor, int, float, str]
-"""Color based on model, predefined values, single greyscale value (0.0 - 1.0) , color index or string representing color (“#RGB”,“#RGBA”,“#RRGGBB”,“#RRGGBBAA”)."""
+"""Color based on provided model.
+
+RGBAColor: RGBA structure value. 
+NamedColor : predefined color
+IndexedColor: single predefined color index with hue.
+int: predefined color index
+float (0.0 - 1.0): greyscale value
+str (“#RGB”,“#RGBA”,“#RRGGBB”,“#RRGGBBAA”): web hex color encoding
+"""
 
 
 @singledispatch
-def _make_color(color: RGBAColor):  # pyright:ignore [reportGeneralTypeIssues]
+def _make_color(color: RGBAColor):  # pyright: ignore [reportGeneralTypeIssues]
     """Convert RGBA color value to QColor"""
     return pg.mkColor((color.R, color.G, color.B, color.A))
 
 
 @singledispatch
-def _make_color(color: IndexedColor):  # pyright:ignore [reportGeneralTypeIssues]
+def _make_color(color: IndexedColor):  # pyright: ignore [reportGeneralTypeIssues]
     """Convert Indexed color value to QColor"""
     return pg.mkColor((color.index, color.hues))
 
 
 @singledispatch
-def _make_color(color: Union[int, float, str]):
+def _make_color(
+    color: Union[int, float, str]
+):  # pyright: ignore [reportGeneralTypeIssues]
+    """Convert Indexed color value to QColor"""
     return pg.mkColor(color)
 
 
@@ -130,12 +141,16 @@ class PenConfig(TypedDict, total=False):
     color: Color
 
 
-# Method to resample the data before plotting ot avoid plotting multiple line segments per pixel.
-DownsampleMethod = Literal[
-    "subsample",  # Downsample by taking the first of N samples. This method is fastest and least accurate.
-    "mean",  # Downsample by taking the mean of N samples.
-    "peak",  # Downsample by drawing a saw wave that follows the min and max of the original data. This method produces the best visual representation of the data but is slower.
-]
+DownsampleMethod = Literal["subsample", "mean", "peak"]
+"""Method to resample the data before plotting to avoid plotting multiple line 
+segments per pixel. 
+
+`subsample`: Downsample by taking the first of N samples. This method is fastest and 
+least accurate.
+`mean`: Downsample by taking the mean of N samples.
+`peak`: Downsample by drawing a saw wave that follows the min and max of the original 
+data. This method produces the best visual representation of the data but is slower.
+"""
 
 
 class ChartWidget:
@@ -156,9 +171,9 @@ class ChartWidget:
         color: Color = RGBAColor(200, 200, 200),
         width: float = 1.0,
         style: PenStyle = PenStyle.SolidLine,
-        autoDownsample: bool = False,
+        auto_downsample: bool = False,
         downsample: int = 1,
-        downsampleMethod: DownsampleMethod = "peak",
+        downsample_method: DownsampleMethod = "peak",
     ):
         """Add a line plot to the chart.
 
@@ -168,15 +183,18 @@ class ChartWidget:
             color (Color): Line color of the plot. Defaults to `RGBAColor(200,200,200)`.
             width (float): Width of the plot line. Defaults to `1.0`.
             style (PenStyle): Style of the plot line. Defaults to `Penstyle.SolidLine`.
-            autoDownsample (bool): If True, resample the data before plotting to avoid plotting multiple line segments per pixel. Defaults to `False`.
-            downsample (int): Reduce the number of samples displayed by the given factor.To disable, set to `1`. Defaults  to `1`.
-            downsampleMethod (DownsampleMethod): Method to use for downsampling. Defaults to `peak`.
+            auto_downsample (bool): If True, resample the data before plotting to avoid
+            plotting multiple line segments per pixel. Defaults to `False`.
+            downsample (int): Reduce the number of samples displayed by the given factor.
+            To disable, set to `1`. Defaults  to `1`.
+            downsample_method (DownsampleMethod): Method to use for downsampling.
+            Defaults to `peak`.
         """
         config = {"name": name}
         config["pen"] = _make_pen(color=color, width=width, style=style)
         plot = self._chart.plot(**config)
         plot.setDownsampling(
-            ds=downsample, auto=autoDownsample, method=downsampleMethod
+            ds=downsample, auto=auto_downsample, method=downsample_method
         )
         self._plots[id] = plot
 
@@ -188,9 +206,9 @@ class ChartWidget:
         color: Color = RGBAColor(50, 50, 150),
         symbol: SymbolType = "o",
         border: PenConfig = PenConfig(color=RGBAColor(200, 200, 200)),
-        autoDownsample: bool = False,
+        auto_downsample: bool = False,
         downsample: int = 1,
-        downsampleMethod: DownsampleMethod = "peak",
+        downsample_method: DownsampleMethod = "peak",
     ):
         """Add a scatter plot to the chart.
 
@@ -198,12 +216,17 @@ class ChartWidget:
             id (str): Unique id of the plot in the chart
             name (str): Name (legend title) of the plot
             size (float): Size of the marker symbol. Defaults to `10.0`.
-            color (Color): Color to fill the marker symbol. Defaults to `RGBAColor(50,50,150)`.
+            color (Color): Color to fill the marker symbol. Defaults to
+            `RGBAColor(50,50,150)`.
             symbol (SymbolType): Shape of the marker symbol. Defaults to `o`.
-            border (PenConfig): Pen to draw border around the marker symbol. Defaults to `PenConfig(color=RGBAColor(200,200,200))`.
-            autoDownsample (bool): If True, resample the data before plotting to avoid plotting multiple line segments per pixel. Defaults to `False`.
-            downsample (int): Reduce the number of samples displayed by the given factor.To disable, set to `1`. Defaults  to `1`.
-            downsampleMethod (DownsampleMethod): Method to use for downsampling. Defaults to `peak`.
+            border (PenConfig): Pen to draw border around the marker symbol. Defaults to
+            `PenConfig(color=RGBAColor(200,200,200))`.
+            auto_downsample (bool): If True, resample the data before plotting to avoid
+            plotting multiple line segments per pixel. Defaults to `False`.
+            downsample (int): Reduce the number of samples displayed by the given
+            factor.To disable, set to `1`. Defaults  to `1`.
+            downsample_method (DownsampleMethod): Method to use for downsampling.
+            Defaults to `peak`.
         """
         config = {
             "name": name,
@@ -216,16 +239,18 @@ class ChartWidget:
 
         plot = self._chart.plot(**config)
         plot.setDownsampling(
-            ds=downsample, auto=autoDownsample, method=downsampleMethod
+            ds=downsample, auto=auto_downsample, method=downsample_method
         )
         self._plots[id] = plot
 
     def set_data(self, id: str, *args, **kwargs):
-        """Update the plot with provided data \n
+        """Update the plot with provided data
+
         set_data(id, x, y):x, y: array_like coordinate values
         set_data(id, y): y values only - x will be automatically set to range(len(y))
         set_data(id, x=x, y=y): x and y given by keyword arguments
-        set_data(id, ndarray(N,2)): single numpy array with shape (N, 2), where x=data[:,0] and y=data[:,1]
+        set_data(id, ndarray(N,2)): single numpy array with shape (N, 2), where
+        x=data[:,0] and y=data[:,1]
 
         Args:
             id (str): Unique id of the plot in the chart
@@ -247,14 +272,19 @@ class ChartsWidget:
 
     Args:
         title (str): Title of charts window.
-        output_file (str, optional): Path of the captured video output file. If not `None`, hides UI. Defaults to `None`.
+        output_file (str, optional): Path of the captured video output file. If not
+        `None`, hides UI. Defaults to `None`.
         output_codec (str): Codec name to be used for encoding. Defaults to `libx264`.
         output_pixel_format (str): Ouput pixel format. Defaults to `yuv420p`.
-        size (Tuple[int,int]):(width,height) of the charts window. Defaults to `1000x1000`.
-        antialias (bool): Use antialiasing. If true, smooth visuals and slower refresh rate. Defaults to `False`.
+        size (Tuple[int,int]):(width,height) of the charts window. Defaults to
+        `1000x1000`.
+        antialias (bool): Use antialiasing. If true, smooth visuals and slower refresh
+        rate. Defaults to `False`.
         foreground (Color): General foreground color (text,ie). Defaults to `d`.
         background (Color): General background color. Defaults to `k`.
-        border (Union[bool,Tuple[int,int,int]], optional): Border between charts.`True` for default border, `False`for None or triplet of int for custom. Defaults to `None`.
+        border (Union[bool,Tuple[int,int,int]], optional): Border between charts.`True`
+        for default border, `False`for None or triplet of int for custom. Defaults to
+        `None`.
     """
 
     def __init__(
@@ -315,11 +345,15 @@ class ChartsWidget:
             title (str, optional): Title to display on the top. Defaults to `None`.
             height (int): Prefered height scale factor.Defaults to `200`.
             legend_width (int): Legend width. Defaults to `300`.
-            x_label (str, optional): Label text to display under x axis. Defaults to `None`.
-            x_range (Tuple[float,float], optional): Constant range(min,max value) of x axis. For dynamic range, set to `None`. Defaults to `None`.
+            x_label (str, optional): Label text to display under x axis. Defaults to
+            `None`.
+            x_range (Tuple[float,float], optional): Constant range(min,max value) of x
+            axis. For dynamic range, set to `None`. Defaults to `None`.
             x_log (bool): Use logarithmic scale to display x axis. Defaults to `False`.
-            y_label (str, optional): Label text to display next to y axis. Defaults to `None`.
-            y_range (Tuple[float,float], optional): Constant range(min,max value) of y axis. For dynamic range, set to `None`. Defaults to `None`.
+            y_label (str, optional): Label text to display next to y axis. Defaults to
+            `None`.
+            y_range (Tuple[float,float], optional): Constant range(min,max value) of y
+            axis. For dynamic range, set to `None`. Defaults to `None`.
             y_log (bool): Use logarithmic scale for y axis. Defaults to `False`.
 
         Returns:
@@ -336,15 +370,19 @@ class ChartsWidget:
         chart.setClipToView(True)
         chart.setLogMode(x=x_log, y=y_log)
 
-        if x_label is not None:
-            chart.setLabel("bottom", x_label)
-        if x_range is not None:
-            chart.setXRange(*x_range)
+        x_label and chart.setLabel(
+            "bottom", x_label
+        )  # pyright: ignore [reportUnusedExpression]
+        x_range and chart.setXRange(
+            *x_range
+        )  # pyright: ignore [reportUnusedExpression]
 
-        if y_label is not None:
-            chart.setLabel("left", y_label)
-        if y_range is not None:
-            chart.setYRange(*y_range)
+        y_label and chart.setLabel(
+            "left", y_label
+        )  # pyright: ignore [reportUnusedExpression]
+        y_range and chart.setYRange(
+            *y_range
+        )  # pyright: ignore [reportUnusedExpression]
 
         self._layout.nextRow()
 
@@ -355,7 +393,8 @@ class ChartsWidget:
         self._app.processEvents()  # type: ignore
 
         if self._save:
-            # Renders a frame by capturing plots drawn on the layout.Re-scales if required based on the size.
+            # Renders a frame by capturing plots drawn on the layout.Re-scales if
+            # required based on the size.
             qimage = self._layout.grab().toImage()
 
             qimage = qimage.convertToFormat(
