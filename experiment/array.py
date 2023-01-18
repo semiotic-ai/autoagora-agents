d96508d56d07e191691b4f00b519fd6d0d1e78d1
Anirudh <anirudh@semiotic.ai>
2023-01-18 10:44:45 -0800

    Array bounds can be themselves arrays

--- experiment/array.py
+++ experiment/array.py
@@ -2,29 +2,44 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import numpy as np
+from numpy.typing import NDArray
 
 
-def inbounds(a: np.ndarray, l: float, h: float) -> np.bool_:
+def inbounds(a: NDArray, l: float | NDArray, h: float | NDArray) -> np.bool_:
     """Check if array is in between lower and upper bounds.
 
     Bounds are inclusive.
 
     Arguments:
-        a (np.ndarray): The array to check
-        l (float): The lower bound
-        h (float): The upper bound
+        a (NDArray): The array to check
+        l (float | NDArray): The lower bound
+        h (float | NDArray): The upper bound
+
+    Returns:
+        bool: True if array is in bounds, else False.
+
+    Raises:
+        ValueError: If length of the bounds don't equal the length of the array, if the
+            bounds are given by arrays.
     """
     return ((a >= l) & (a <= h)).all()
 
 
-def applybounds(a: np.ndarray, l: float, h: float) -> np.ndarray:
+def applybounds(a: NDArray, l: float | NDArray, h: float | NDArray) -> NDArray:
     """Set out of bounds values to be between bounds.
 
     Bounds are inclusive.
 
     Arguments:
-        a (np.ndarray): The array to which to apply bounds.
-        l (float): The lower bound
-        h (float): The upper bound
+        a (NDArray): The array to which to apply bounds.
+        l (float | NDArray): The lower bound
+        h (float | NDArray): The upper bound
+
+    Returns:
+        NDArray: The input array with the out of bounds values set to be in bounds.
+
+    Raises:
+        ValueError: If length of the bounds don't equal the length of the array, if the
+            bounds are given by arrays.
     """
     return np.minimum(np.maximum(a, l), h)
--- tests/experiment/test_array.py
+++ tests/experiment/test_array.py
@@ -2,21 +2,36 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import numpy as np
+import pytest
 
 import experiment
 
 
-def test_inbounds():
+def test_inbounds_float():
     a = np.array([1, 2, 3])
     l = 0
     h = 3
     assert experiment.inbounds(a, l, h)
 
 
-def test_not_inbounds():
+def test_inbounds_array():
+    a = np.array([1, 2, 3])
+    l = np.array([0, 1, 2])
+    h = np.array([2, 3, 4])
+    assert experiment.inbounds(a, l, h)
+
+
+def test_not_inbounds_float():
     a = np.array([1, 2, 3])
     l = 0
     h = 2
+    assert not experiment.inbounds(a, l, h)
+
+
+def test_not_inbounds_array():
+    a = np.array([1, 2, 3])
+    l = np.array([2, 3, 4])
+    h = np.array([2, 3, 4])
     assert not experiment.inbounds(a, l, h)
 
 
@@ -27,10 +42,25 @@ def test_inbounds_empty():
     assert experiment.inbounds(a, l, h)
 
 
-def test_applybounds():
+def test_inbounds_raises_valueerror():
+    a = np.array([])
+    l = np.array([1, 2])
+    h = 2
+    with pytest.raises(ValueError):
+        _ = experiment.inbounds(a, l, h)
+
+
+def test_applybounds_float():
     a = np.array([0, 2, 4])
     l = 1
     h = 3
+    assert experiment.inbounds(experiment.applybounds(a, l, h), l, h)
+
+
+def test_applybounds_array():
+    a = np.array([1, 2, 3])
+    l = np.array([2, 3, 4])
+    h = np.array([2, 3, 4])
     assert experiment.inbounds(experiment.applybounds(a, l, h), l, h)
 
 
@@ -39,3 +69,11 @@ def test_applybounds_empty():
     l = 1
     h = 3
     assert experiment.inbounds(experiment.applybounds(a, l, h), l, h)
+
+
+def test_applybounds_raises_valueerror():
+    a = np.array([])
+    l = np.array([1, 2])
+    h = 3
+    with pytest.raises(ValueError):
+        _ = experiment.applybounds(a, l, h)
