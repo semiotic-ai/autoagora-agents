2093431cbd275427764823ca05f20bf80f0c7e42
Anirudh <anirudh@semiotic.ai>
2023-02-05 15:23:57 -0800

    Implement observation via decorators

---
+++ tests/simulation/test_observation.py
@@ -0,0 +1,19 @@
+# Copyright 2022-, Semiotic AI, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+
+from simulation.entity.entity import entitygroupfactory
+from simulation import observation
+from ..fixture import *
+
+
+def test_bandit_observation(agentconfig):
+    agentconfig["observation"] = [
+        {"kind": "bandit"},
+    ]
+    entities = {"indexer": entitygroupfactory(**agentconfig)}
+    obs = observation.observationfactory(observations=agentconfig["observation"])
+    assert isinstance(obs, observation.BanditObservation)
+    agent = entities["indexer"][0]
+    assert np.allclose(obs(agent=agent, entities=entities), np.array([]))  # type: ignore
